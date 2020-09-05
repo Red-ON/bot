10a5079324dc13ab5dcc1a3012299bd1b39d75f4
Redonpati <70238895+Redonpati@users.noreply.github.com>
2020-09-05 23:09:13 +0200

    bot

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, ops) => {
   helpMsg = new Discord.MessageEmbed()
   .setTitle(client.user.username)
-  .setDescription("Tutti i comandi per la musica del bot**" + client.user.username + "**")
+  .setDescription("Tutti i comandi per la musica del bot **" + client.user.username + "**")
   .addField("🔊 Music", "`play`, `leave`, `pause`, `resume`, `search`, `skip`, `musiclink`, `volume`")
   .setColor('00e3ff')
   message.channel.send(helpMsg);

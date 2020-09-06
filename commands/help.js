13a4a802cf3d61fd659f1c0c9ea0be2b8dc966ca
Redonpati <70238895+Redonpati@users.noreply.github.com>
2020-09-07 00:34:59 +0200

    bot

--- commands/help.js
+++ commands/help.js
@@ -4,10 +4,10 @@ const Discord = require('discord.js');
 exports.run = (client, message, args, ops) => {
   helpMsg = new Discord.MessageEmbed()
   .setTitle(client.user.username)
-  .setDescription("**Tutti i comandi disponibili per il bot**" + client.user.username + "**")
-  .addField("👮‍ Moderation", "`clear` `ban` `kick` `nick` `mute` `unmute` `whois`")
+  .setDescription("**Tutti i comandi disponibili per il bot** **" + client.user.username + "**")
+  .addField("👮‍ Moderation", "`clear` `ban` `kick` `nick` `mute` `unmute`")
   .addField("🔥 Fun and Misc", "`help` `invite` `mcstatus` `mcskin` `skin` `8ball` `meme` `mcleaks` `ping`")
-  .addField("🚀 Utility", "`covid19`say`")
+  .addField("🚀 Utility", "`covid19` `say`")
   .setColor('00adff')
   message.channel.send(helpMsg);
 }
